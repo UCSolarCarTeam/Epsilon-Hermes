335abe4a5710f1e060e4f70839502a94aae3327b
magnus <mglyngberg2@gmail.com>
2020-03-18 20:01:10 -0600

    Formatting

--- src/BusinessLayer/JsonForwarder/JsonDefines.h
+++ src/BusinessLayer/JsonForwarder/JsonDefines.h
@@ -180,7 +180,7 @@ namespace JsonFormat
     const QString CONTACTOR_ERROR = "ContactorError";
     const QString HIGH_VOLTAGE_ENABLE = "HighVoltageEnable";
     const QString CHARGE_TRIP_DUE_TO_HIGH_CELL_VOLTAGE = "ChargeTripDueToHighCellVoltage";
-    const QString CHARGE_TRIP_DUE_TO_HIGH_TEMPERATURE_AND_CURRENT ="ChargeTripDueToHighTemperatureAndCurrent";
+    const QString CHARGE_TRIP_DUE_TO_HIGH_TEMPERATURE_AND_CURRENT = "ChargeTripDueToHighTemperatureAndCurrent";
     const QString CHARGE_TRIP_DUE_TO_PACK_CURRENT = "ChargeTripDueToPackCurrent";
     const QString DISCHARGE_TRIP_DUE_TO_LOW_CELL_VOLTAGE = "DischargeTripDueToLowCellVoltage";
     const QString DISCHARGE_TRIP_DUE_TO_HIGH_TEMERATURE_AND_CURRENT = "DischargeTripDueToHighTemeratureAndCurrent";
